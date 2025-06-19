37fc711d5c951a0325d58d23ab8467f11b7270ff
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-06-19 19:36:28 +0530

    add some features

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,46 +1,106 @@
 import { useState } from "react";
-import "./todoList.css"
-export default function TotoList(){
+import { v4 as uuidv4 } from "uuid";
 
-    let [todos , setTodos] = useState(["sample todo"]);
-    let [newTodo , setNewTodo] = useState("");
+import "./todoList.css";
+export default function TotoList() {
+  let [todos, setTodos] = useState([{ task: "sample todo", id: uuidv4() }]);
+  let [newTodo, setNewTodo] = useState("");
 
-    let addTodo = () => {
-      setTodos(
-        [...todos , newTodo])
-        setNewTodo("");
-    }
+  let addTodo = () => {
+    setTodos((prevTodos) => {
+      return [...prevTodos, { task: newTodo, id: uuidv4(), isDone: false }];
+    });
+    setNewTodo("");
+  };
 
-    let updateTodo = (event) => {
-        setNewTodo(event.target.value);
-    }
-    return(
-        <>
-           <div className="todo-container">
-            <h1 className="todo-heading" >TodoList</h1>
-            <br /><br />
-            <div className="input-container">
-        <input
-          type="text"
-          value={newTodo}
-          onChange={updateTodo}
-          placeholder="Enter a new task..."
-          className="todo-input"
-        />
-        <button className="add-button" onClick={addTodo}>
-          ➕ Add
-        </button>
-      </div>
-            <hr />
-            <br />
-            <ul className="todo-list">
-                {
-                    todos.map( (todo) => {
-                        return <li className="todo-item">{todo}</li>
-                    })
-                }
-            </ul>
-            </div>
-        </>
+  let updateTodo = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  let deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id != id));
+  };
+
+  let toUpperCase = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            task: todo.task.toUpperCase(),
+          };
+        } else {
+          return todo;
+        }
+      })
     );
+  };
+
+  let isDone = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+  let getTodoItemStyle = (isDone) => {
+    return isDone ? { textDecoration: "line-through" } : {};
+  };
+
+  return (
+    <>
+      <div className="todo-container">
+        <h1 className="todo-heading">TodoList</h1>
+        <br />
+        <br />
+        <div className="input-container">
+          <input
+            type="text"
+            value={newTodo}
+            onChange={updateTodo}
+            placeholder="Enter a new task..."
+            className="todo-input"
+          />
+          <button className="add-button" onClick={addTodo}>
+            ➕ Add
+          </button>
+        </div>
+        <hr />
+        <br />
+        <ul className="todo-list">
+          {todos.map((todo) => {
+            return (
+              <li className="todo-item" key={todo.id}>
+                <span style={getTodoItemStyle(todo.isDone)}>{todo.task}</span>
+                &nbsp;
+                <button
+                  className="delete_btn"
+                  onClick={() => deleteTodo(todo.id)}
+                >
+                  Delete
+                </button>
+                &nbsp;&nbsp;
+                <button
+                  className="edit_btn"
+                  onClick={() => toUpperCase(todo.id)}
+                >
+                  ToUpperCase
+                </button>
+                <button className="isDone_btn" onClick={() => isDone(todo.id)}>
+                  {todo.isDone ? "Undo" : "Done"}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
